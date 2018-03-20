bb390cfd87a121d11a7d4fc79d4993ad0842ac12
bushmakinoy <bushmakinoy@dom.mos.ru>
2018-03-20 20:57:15 +0300

    Revert "ллллл"
    This reverts commit 019b18c6a8906d7221870d17325975c8d151cb33.

--- catel1/Models/mainModel.Designer.cs
+++ catel1/Models/mainModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Олег\Documents\GitHub\catel1\catel1\Models\mainModel.edmx" включено. 
+﻿// Создание кода T4 для модели "E:\bushmakinoy\Visual Studio 2015\Projects\catel1\catel1\Models\mainModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
